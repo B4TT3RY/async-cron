4e4ded3e559f71f76e92d726703211266b72ebcc
B4TT3RY <gb4tt3ry@gmail.com>
2021-04-19 23:26:53 +0900

    Remove dependencies

--- src/job.rs
+++ src/job.rs
@@ -2,17 +2,14 @@ use futures::Stream;
 use std::{pin::Pin, str::FromStr, task::{Context, Poll}};
 
 use cron::Schedule;
-use uuid::Uuid;
 
 pub struct Job {
-    id: Uuid,
     schedule: Schedule,
 }
 
 impl Job {
     pub fn new(expression: &str) -> Result<Self, cron::error::Error> {
         Ok(Job {
-            id: Uuid::new_v4(),
             schedule: Schedule::from_str(expression)?,
         })
     }
