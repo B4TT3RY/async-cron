5de5ea42d9894ea0eb0b66f4bf540b6c60672e30
B4TT3RY <gb4tt3ry@gmail.com>
2021-04-20 00:28:29 +0900

    Testing poll_next()

--- src/job.rs
+++ src/job.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Utc};
 use futures::Stream;
 use std::{pin::Pin, str::FromStr, task::{Context, Poll}};
 
@@ -5,12 +6,14 @@ use cron::Schedule;
 
 pub struct Job {
     schedule: Schedule,
+    last_tick: Option<DateTime<Utc>>,
 }
 
 impl Job {
     pub fn new(expression: &str) -> Result<Self, cron::error::Error> {
         Ok(Job {
             schedule: Schedule::from_str(expression)?,
+            last_tick: None,
         })
     }
 }
@@ -20,8 +23,32 @@ impl Stream for Job {
 
     fn poll_next(
         self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
+        _cx: &mut Context<'_>,
     ) -> Poll<Option<Self::Item>> {
-        todo!()
+        let now = Utc::now();
+        
+        if self.last_tick.is_none() {
+            self.get_mut().last_tick = Some(now);
+            println!("last_tick isn't set");
+            return Poll::Ready(Some(()));
+        }
+
+        let event = self
+            .schedule
+            .after(&self.last_tick.unwrap())
+            .take(1)
+            .next()
+            .unwrap();
+
+        if event > now {
+            println!("event: {}, now: {}", event, now);
+            return Poll::Ready(Some(()));
+        }
+
+        Poll::Ready(Some(()))
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (0, None)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,20 @@ pub mod job;
 
 #[cfg(test)]
 mod tests {
+    use futures::StreamExt;
+
     #[allow(unused_imports)]
     use crate::job::Job;
 
     #[tokio::test]
     async fn create_job_tokio() {
-        
+        let mut job = Job::new("* * * * * *").unwrap();
+
+        while let Some(()) = job.next().await {
+            println!("Hi~~~");
+        }
+
+        println!("Tokio End");
     }
 
     #[async_std::test]
